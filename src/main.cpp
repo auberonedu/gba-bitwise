2b78bc8d4ba418e1ea57585519428ea20a61a470
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-02-12 19:51:06 +0000

    added all the binary work as done in class

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,26 @@ int main()
 
     int bin_num = 0b101101;
     int mask = 0b101011;
-    // int anded = bin_num & mask;
+    int anded = bin_num & mask;
     int ored = bin_num | mask;
 
-    // int num = 86;
+    // 15 bit colors represented in 5 bits for each color (Blue, Green, Red)
+    int color = 0b0'00011'00011'01001;
+    int blue = 6 << 10;  // moves the blue component to the correct position
+    int green = 15 << 5; // moves the green component to the correct position
+    int red = 20;        // moves the red component to the correct position
+
+    color = blue | green | red;
 
     BN_LOG(to_string_base(ored, 2));
+    BN_LOG(to_string_base(anded, 2));
     BN_LOG(bin_num);
     BN_LOG("Hey!");
+    BN_LOG(to_string_base(blue, 2));
+    BN_LOG(to_string_base(green, 2));
+    BN_LOG(to_string_base(red, 2));
 
-    bn::backdrop::set_color(bn::color());
+    bn::backdrop::set_color(bn::color(color));
 
     while (true)
     {
