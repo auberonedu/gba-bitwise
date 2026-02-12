995a437103fa0846640954f48398ecdabffe43c9
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-02-12 19:49:05 +0000

    Add color operations

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,32 @@
 int main() {
     bn::core::init();
 
-    int num = 29;
-    BN_LOG(to_string_base(num, 2));
+    int bin_num = 0b10101011110001;
+    int mask =    0b01110001001010;
+
+    int and_num = bin_num & mask;
+    int or_num = bin_num | mask;
+
+    BN_LOG(to_string_base(and_num, 2));
+    BN_LOG(to_string_base(or_num, 2));
+    //int num = 29;
+    //BN_LOG(bin_num);
+    //BN_LOG(to_string_base(num, 2));
 
     BN_LOG("Hey!");
 
-    bn::backdrop::set_color(bn::color());
+    int color = 0; //0b0'00011'00110'01001;
+    int blue = 6 << 10;
+    int green = 15 << 5;
+    int red = 20;
+
+    color = blue | green | red;
+
+    BN_LOG(to_string_base(blue, 2));
+    BN_LOG(to_string_base(green, 2));
+    BN_LOG(to_string_base(red, 2));
+
+    bn::backdrop::set_color(bn::color(color));
 
     while(true) {
         bn::core::update();
