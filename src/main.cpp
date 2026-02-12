0511f8fe7834a385c80672ad9156a67f601afbc9
JPbooya <perezjcarlos81@gmail.com>
2026-02-12 19:51:25 +0000

    added colors using bitwise operations and binary numbers

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,34 @@
 int main() {
     bn::core::init();
 
-    BN_LOG("Hey!");
+    int bin_num = 0b100010;
+    int mask =    0b011100;
 
-    bn::backdrop::set_color(bn::color());
+    int anded = bin_num & mask;
+    int ored = bin_num | mask;
+
+    BN_LOG(to_string_base(anded, 2));
+    BN_LOG(to_string_base(ored, 2));
+
+    // int num = 86;
+
+
+   // BN_LOG(to_string_base(num, 2));
+
+    BN_LOG("Hey!");
+                //  blue  green  red
+    int color = 0;// 0b1'00011'00011'00111;
+    int blue = 6 << 10;
+    int green = 15 << 5;
+    int red = 20;
+
+    BN_LOG(to_string_base(blue, 2));
+    BN_LOG(to_string_base(green, 2));
+    BN_LOG(to_string_base(red, 2));
+
+    color = blue | green | red;
+
+    bn::backdrop::set_color(bn::color(color));
 
     while(true) {
         bn::core::update();
